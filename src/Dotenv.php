fb77b659ed90c32e77b87e0fbcade5ba1a54f416
Matti van de Weem <mattiweem@hotmail.com>
2015-01-04 22:24:38 +0100

    set up class and the construction

--- src/Dotenv.php
+++ src/Dotenv.php
@@ -3,5 +3,31 @@
 
 class Dotenv{
 
+ 	/**
+	 * The file types the library opens to check contents
+	 * @var array
+	 */
+	protected static $fileTypes = array(
+		'.env',
+		'.json',
+	);
+
+
+	/**
+	 *
+	 * Matches a path against an array.
+	 * If given path is folder scope for files and return array
+	 *
+	 *
+	 * @param $path
+	 * @param $scope Once the path is an folder, read trough all files in given dir.
+	 * @param $scopeLimit ammount of sub dirs to scope for files.
+	 * @throws \RuntimeException
+	 * @return int
+	 *
+	 */
+	public static function fileType($path, $scope = true, $scopeLimit = 3){
+
+	}
 }
 
