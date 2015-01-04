4e3601093534d06fa3feacaec7cecb6c01098f01
Matti van de Weem <mattiweem@hotmail.com>
2015-01-04 21:55:37 +0100

    setup for default php development envoirment

---
+++ tests/Dotenv/Dotenv.php
@@ -0,0 +1,16 @@
+<?php
+class DotenvTest extends \PHPUnit_Framework_TestCase
+{
+
+	/*
+	public function testDotenvLoadsEnvironmentVars()
+    {
+        Dotenv::load(dirname(__DIR__) . '/fixtures');
+        $this->assertEquals('bar', getenv('FOO'));
+        $this->assertEquals('baz', getenv('BAR'));
+        $this->assertEquals('with spaces', getenv('SPACED'));
+        $this->assertEquals('', getenv('NULL'));
+    }
+	*/
+
+}
