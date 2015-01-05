8623977c4be7fe512e3afb9bc3d05db45af11973
Matti van de Weem <mattiweem@hotmail.com>
2015-01-05 12:26:31 +0100

    load up files and check their extension

--- src/Dotenv.php
+++ src/Dotenv.php
@@ -9,25 +9,86 @@ class Dotenv{
 	 */
 	protected static $fileTypes = array(
 		'.env',
-		'.json',
+		'.json'
 	);
 
 
 	/**
+	 * Ammount of levels to scope 0 = infinite
+	 * @var int
+	 */
+	protected static $scope = 3;
+
+	/**
+	 *
+	 * load the given file in to the system
+	 * @param path
+	 * @param scopes
+	 * @return this
+	 *
+	 */
+	public function load($path,$scopes = 0){
+
+		$type = $this->fileType($path);
+		if(static::$scope === 0):
+			$limit = true;
+		else:
+			$limit = $scopes < static::$scope;
+		endif;
+
+		if($type === '@' && $limit):
+			$scopes++;
+			foreach(glob($path.'/*') as $file):
+				$this->load($file, $scopes);
+			endforeach;
+		endif;
+
+		echo $this->fileType($path."\n");
+		return $this;
+
+	}
+
+	public function readFile($path){
+
+	}
+
+	/**
 	 *
 	 * Matches a path against an array.
-	 * If given path is folder scope for files and return array
 	 *
 	 *
 	 * @param $path
-	 * @param $scope Once the path is an folder, read trough all files in given dir.
-	 * @param $scopeLimit ammount of sub dirs to scope for files.
 	 * @throws \RuntimeException
 	 * @return int
 	 *
 	 */
-	public static function fileType($path, $scope = true, $scopeLimit = 3){
+	public static function fileType($path){
+		if(is_dir($path)):
+			return '@';
+		endif;
 
+		$extension = '.'.pathinfo($path, PATHINFO_EXTENSION);
+		$fileTypes = static::$fileTypes;
+		if(in_array($extension, $fileTypes)):
+			foreach($fileTypes as $key => $value):
+				if($extension == $value):
+					return $key;
+				endif;
+			endforeach;
+		endif;
+
+	}
+
+
+
+	/**
+	 * set scoping level
+	 * @param int
+	 * @return this
+	 */
+ 	public function setScope($setScope){
+		static::$scope = $setScope;
+		return $this;
 	}
 }
 
