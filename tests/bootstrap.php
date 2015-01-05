cd5b34ef2d1f19cd31041006bd7ce5c2797b5278
Matti van de Weem <mattiweem@hotmail.com>
2015-01-05 11:20:21 +0100

    clean up

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,10 +7,10 @@ date_default_timezone_set('UTC');
 */
 $vendorPos = strpos(__DIR__, 'vendor/MattivdWeem/phpdotenv');
 if($vendorPos !== false) {
-// Package has been cloned within another composer package, resolve path to autoloader
-$vendorDir = substr(__DIR__, 0, $vendorPos) . 'vendor/';
-$loader = require $vendorDir . 'autoload.php';
+	// Package has been cloned within another composer package, resolve path to autoloader
+	$vendorDir = substr(__DIR__, 0, $vendorPos) . 'vendor/';
+	$loader = require $vendorDir . 'autoload.php';
 } else {
-// Package itself (cloned standalone)
-$loader = require __DIR__.'/../vendor/autoload.php';
+	// Package itself (cloned standalone)
+	$loader = require __DIR__.'/../vendor/autoload.php';
 }
